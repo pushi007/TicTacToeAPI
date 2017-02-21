cd51675bfbfce4f753aa2f9a413d2b493fc9bcf8
pushi007 <praveenpal33@gmail.com>
2017-02-21 10:57:20 -0600

    updated

--- src/com/moya/api/async/Task.java
+++
@@ -1,8 +0,0 @@
-package com.moya.api.async;
-
-import java.util.concurrent.Callable;
-import java.util.concurrent.FutureTask;
-
-public abstract class Task<T> implements Callable<FutureTask<T>>{
-
-}
